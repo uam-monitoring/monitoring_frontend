60ef100463c327069014e8515897cc5b6eb37b26
심재헌 <zh025700@naver.com>
2023-03-27 23:24:45 +0900

    feat: render map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Map from "react-map-gl";
+import DeckGL from "@deck.gl/react";
+const INITIAL_VIEW_STATE = {
+  longitude: 127.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoiamFlaGVvbiIsImEiOiJjbGZxdmR6eHYwMmc0NDRsbDI2dWRnYjc1In0.CgjNkZEQHphOCvirgEekqA";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true}>
+      <Map
+        mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </DeckGL>
   );
 }
-
 export default App;
